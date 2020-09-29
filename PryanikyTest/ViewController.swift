95543af26dada9ed9c5bba63164bfc935863233b
Ilyas <zhumadillov@gmail.com>
2020-09-29 12:56:15 +0600

    Initial Commit

---
+++ PryanikyTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PryanikyTest
+//
+//  Created by Ilyas Zhumadilov on 29.09.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
